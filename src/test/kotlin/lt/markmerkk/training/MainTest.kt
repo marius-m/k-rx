801579d3d492b9ff114ebfeb2cbce99a71a89d59
Marius Merkevicius <marius.merkevicius@wahanda.com>
2017-11-09 21:00:28 +0200

    Basic

--- src/test/kotlin/lt/markmerkk/training/MainTest.kt
+++ src/test/kotlin/lt/markmerkk/training/MainTest.kt
@@ -1,7 +1,10 @@
 package lt.markmerkk.training
 
-import org.assertj.core.api.Assertions.assertThat
+import io.reactivex.Observable
+import io.reactivex.disposables.Disposable
 import org.junit.Test
+import org.reactivestreams.Subscription
+import java.util.concurrent.TimeUnit
 
 /**
  * @author mariusmerkevicius
@@ -10,11 +13,108 @@ import org.junit.Test
 class MainTest {
 
     @Test
-    fun valid() {
-        // Assemble
-        // Act
-        assertThat(false).isFalse()
-
-        // Assert
+    fun just_1() {
+        Observable.just(1)
+                .subscribe({
+                    value -> println("onNext:$value")
+                }, {
+                    error -> println("onError:$error" )
+                }, {
+                    println("onComplete")
+                })
     }
+
+    @Test
+    fun from() {
+        val subscription: Disposable = Observable.fromArray(1, 2, 3, 4, 5)
+                .subscribe({ value ->
+                    println("onNext:$value")
+                }, { error ->
+                    println("onError:$error")
+                }, {
+                    println("onComplete")
+                })
+    }
+
+    @Test
+    fun error() {
+        Observable.error<Any>(RuntimeException())
+                .subscribe({
+                    value -> println("onNext:$value")
+                }, {
+                    error -> println("onError:$error" )
+                }, {
+                    println("onComplete")
+                })
+    }
+
+    @Test
+    fun empty() {
+        Observable.empty<Any>()
+                .subscribe({
+                    value -> println("onNext:$value")
+                }, {
+                    error -> println("onError:$error" )
+                }, {
+                    println("onComplete")
+                })
+    }
+
+    @Test
+    fun never() {
+        Observable.never<Any>()
+                .subscribe({
+                    value -> println("onNext:$value")
+                }, {
+                    error -> println("onError:$error" )
+                }, {
+                    println("onComplete")
+                })
+    }
+
+    @Test
+    fun create() {
+        Observable.create<String>({ observable ->
+            observable.onNext("value_1")
+            observable.onNext("value_2")
+            observable.onNext("value_3")
+            observable.onComplete()
+        }).subscribe({ value ->
+            println("onNext:$value")
+        }, { error ->
+            println("onError:$error")
+        }, {
+            println("onComplete")
+        })
+    }
+
+    @Test
+    fun interval() {
+        Observable.interval(1L, TimeUnit.SECONDS)
+                .subscribe({ value ->
+                    println("onNext:$value")
+                }, { error ->
+                    println("onError:$error")
+                }, {
+                    println("onComplete")
+                })
+        Thread.sleep(5000)
+    }
+
+    @Test
+    fun interval_withComplete() {
+        val subscription: Disposable = Observable.interval(1L, TimeUnit.SECONDS)
+                .subscribe({ value ->
+                    println("onNext:$value")
+                }, { error ->
+                    println("onError:$error")
+                }, {
+                    println("onComplete")
+                })
+        Thread.sleep(3000)
+        subscription.dispose()
+        println("Disposed")
+        Thread.sleep(3000)
+    }
+
 }
