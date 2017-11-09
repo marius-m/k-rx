24963146fb21354f04253fdc9fe70b82324ee003
Marius Merkevicius <marius.merkevicius@wahanda.com>
2017-11-09 20:30:49 +0200

    Intitial commit

---
+++ src/main/kotlin/lt/markmerkk/training/Main.kt
@@ -0,0 +1,5 @@
+package lt.markmerkk.training
+
+class Main {
+
+}
---
+++ src/test/kotlin/lt/markmerkk/training/MainTest.kt
@@ -0,0 +1,20 @@
+package lt.markmerkk.training
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+/**
+ * @author mariusmerkevicius
+ * @since 2017-11-09
+ */
+class MainTest {
+
+    @Test
+    fun valid() {
+        // Assemble
+        // Act
+        assertThat(false).isFalse()
+
+        // Assert
+    }
+}
